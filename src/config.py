5f40063a952831d35513b92fc365c634ab0bbe6b
Lorttexwolf <comisgammer@gmail.com>
2023-02-14 22:45:38 -0600

    Provide default value of REDIS_HOST.

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ SERVER_PORT = 8002
 AUTH = env.get("BOT_API_AUTH", "")
 DEBUG_MODE = env.get("PROD") != "TRUE"
 
-REDIS_HOST = env.get("REDIS_HOST")
+REDIS_HOST = env.get("REDIS_HOST", "")
 REDIS_PORT = 6379
 REDIS_PASSWORD = env.get("REDIS_PASSWORD", "")
 MONGO_URL = env.get("MONGO_URL", "")
