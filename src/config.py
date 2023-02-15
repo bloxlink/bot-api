d900979f6a18f22ea1594b3f48dcf6b1038ada60
Lorttexwolf <comisgammer@gmail.com>
2023-02-14 22:38:02 -0600

    Added config env variables.

--- src/config.py
+++ src/config.py
@@ -2,10 +2,10 @@ from os import environ as env
 
 SERVER_HOST = "0.0.0.0"
 SERVER_PORT = 8002
-AUTH = env.get("BOT_API_AUTH", "oof")
+AUTH = env.get("BOT_API_AUTH", "")
 DEBUG_MODE = env.get("PROD") != "TRUE"
 
-REDIS_HOST = ""
+REDIS_HOST = env.get("REDIS_HOST")
 REDIS_PORT = 6379
-REDIS_PASSWORD = ""
-MONGO_URL  = ""
+REDIS_PASSWORD = env.get("REDIS_PASSWORD", "")
+MONGO_URL = env.get("MONGO_URL", "")
